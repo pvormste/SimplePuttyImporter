5b664e5aecd13b44187d32e6372e1a7ea0ad6178
Patric Vormstein <patric@vormstein.de>
2014-11-28 15:24:47 +0100

    Initial commit

---
+++ ExportWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimplePuttyImporter
+{
+    /// <summary>
+    /// Interaction logic for ExportWindow.xaml
+    /// </summary>
+    public partial class ExportWindow : Window
+    {
+        public ExportWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Clicked(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
